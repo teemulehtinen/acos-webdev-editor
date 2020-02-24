dfc14af87c5047ea0c9952b420bbdbeadbf499d8
giacomo <mariang1@aalto.fi>
2020-02-24 17:16:36 +0200

    changed function name in exercise

--- content.js
+++ content.js
@@ -95,6 +95,24 @@ let Content = {
     title: "declare doubleNumber function",
     concepts: ["JavaScrit", "function declaration"],
     order: 3
+  },
+  half_number_function: {
+    instructions: "Define a function called <code>halveNumber</code>, that takes a number as parameter and returns the given number divided by 2",
+    initialJs: '',
+    postExecuteJs: 'var a = Math.floor((Math.random() * 100) + 1);\n'
+    + 'display.cmd("halveNumber(" + a + ")");\n'
+    + 'display.res(halveNumber(a), [a]);',
+    executeAtStart: false,
+    points: function ($element, config, accessor) {
+      var p = accessor.testResults(10, function (i, args, res) {
+        return parseFloat(res) === args[0]/2 ? 10 : 0;
+      });
+      return { points: p }
+    },
+    maxPoints: 10,
+    title: "declare halveNumber function",
+    concepts: ["JavaScrit", "function declaration"],
+    order: 3
   }
 };
 
